876c3be5660c8bfab9c6b40a1fbf0104f917dfad
xLuaR <superkingjoshy@gmail.com>
2026-05-12 22:17:15 +0800

    Update latestScriptVersion.lua

--- Granny's House Online/latestScriptVersion.lua
+++ Granny's House Online/latestScriptVersion.lua
@@ -2,8 +2,8 @@
 -- using io.open() && io.write() to store and write the obtained raw content.
 -- using loadfile() to load the stored raw content. or use load() to directly load the raw code content.
 
-local latestVersion = "5.0.0"
-local whatsNew = "• Test1\n• Test2"
+local latestVersion = "5.0.1"
+local whatsNew = "[🧰] Just fixed a few errors in the script code that caused slow initialization. There's nothing much new, but still, stay tuned for more updates! :D"
 
 return {
     ["latestVersion"] = latestVersion,
